bf43c2f85590a4e75e5354b49af412f56e54d35c
Yury Popenko <popenko1337@gmail.com>
2023-10-25 15:45:29 +0700

    server.go - create report generator

--- reporter/cmd/server.go
+++ reporter/cmd/server.go
@@ -3,13 +3,18 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	_ "image/jpeg"
 	"io"
 	"log"
 	"net"
 	"net/http"
-	"os"
+	_ "net/http/pprof"
 	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
 	"time"
 
 	reporter "reporter/pkg/reporter/service"
@@ -18,58 +23,216 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/xuri/excelize/v2"
 )
 
 const (
-	apiKey = "IJDHKmA5qlYGvBjD/CyPRw==3CIIQF9Nmym5mVwB"
-	url    = "https://api.api-ninjas.com/v1/randomimage"
-	accept = "image/jpg"
+	// Random Image API
+	riApiKey = "IJDHKmA5qlYGvBjD/CyPRw==3CIIQF9Nmym5mVwB"
+	riUrl    = "https://api.api-ninjas.com/v1/randomimage"
+	riAccept = "image/jpg"
 
-	grpcPort  = 50052
-	message   = "hmm, what time is it?"
-	address   = "localhost:50051"
-	chunkSize = 1024
+	// FishText API
+	ftNumber = 500
+	ftUrl    = "https://fish-text.ru/get"
+	ftType   = "sentence"
 
+	// Timer gRPC
+	grpcPort        = 50052
+	address         = "localhost:50051"
 	defaultLocation = "Asia/Novosibirsk"
+
+	url = "http://localhost:3000/download"
 )
+
+type ftResponse struct {
+	Status    string `json:"status"`
+	Text      string `json:"text"`
+	ErrorCode int    `json:"errorCode"`
+}
 
 type server struct {
 	reporter.UnimplementedReporterServer
 }
 
-func (s *server) GetReport(req *reporter.GetRequest, stream reporter.Reporter_GetReportServer) error {
-	time, err := getLocalTime()
-	if err != nil {
-		log.Print("failed to get local time")
-		return err
+func (s *server) GetReport(_ *reporter.GetRequest, stream reporter.Reporter_GetReportServer) error {
+	log.Println("1 new connection")
+
+	timech := make(chan string, 1)
+	textch := make(chan string, 1)
+	imagech := make(chan []byte, 1)
+	errch := make(chan error, 3)
+
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	go func(ch chan<- string, ech chan<- error) {
+		defer wg.Done()
+		t, err := getLocalTime()
+		if err != nil {
+			log.Println("failed to get local time")
+			ech <- err
+		}
+		ch <- t
+		close(ch)
+	}(timech, errch)
+
+	go func(ch chan<- []byte, ech chan<- error) {
+		defer wg.Done()
+		img, err := getRandomImage()
+		if err != nil {
+			log.Println("failed to get random image")
+			ech <- err
+		}
+		ch <- img
+		close(ch)
+	}(imagech, errch)
+
+	go func(ch chan<- string, ech chan<- error) {
+		defer wg.Done()
+		text, err := getFishText()
+		if err != nil {
+			log.Println("failed to get fish text")
+			ech <- err
+		}
+		ch <- text
+		close(ch)
+	}(textch, errch)
+
+	wg.Wait()
+
+	if len(errch) != 0 {
+		log.Println("error: ", <-errch)
+		close(errch)
+		return fmt.Errorf("failed to get report")
 	}
 
-	err = sendLocalTime(time, stream)
+	t := <-timech
+	buf, err := getReport(t, <-imagech, <-textch, stream)
 	if err != nil {
-		log.Print("failed to send local time")
-		return err
-	}
+		log.Println("failed to generate report")
+		return fmt.Errorf("failed to get report")
 
-	data, err := getRandomImage()
-	if err != nil {
-		log.Print("failed to get random image")
-		return err
 	}
+	fileName := fmt.Sprintf("%s.xlsx", t)
+	contentDisposition := fmt.Sprintf("attachment; filename=%s", fileName)
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Content-Disposition", contentDisposition)
+		w.Header().Set("Content-Transfer-Encoding", "binary")
+		w.Header().Set("Expires", "0")
 
-	path := filepath.Join("storage", fmt.Sprint(time, ".jpg"))
-	err = saveDataToFile(data, path)
-	if err != nil {
-		log.Print("failed to save image")
-		return err
-	}
+		_, err = io.Copy(w, buf)
+		if err != nil {
+			log.Println("failed to copy file")
+		}
+	})
 
-	err = sendDataToStream(data, stream)
-	if err != nil {
-		log.Print("failed to send image")
-		return err
-	}
+	go func() {
+		log.Fatal(http.ListenAndServe(":3000", nil))
+	}()
 
+	err = stream.Send(&reporter.GetResponse{
+		Res: &reporter.GetResponse_Url{
+			Url: url,
+		},
+	})
 	return nil
+}
+
+func getReport(time string, image []byte, text string, stream reporter.Reporter_GetReportServer) (*bytes.Buffer, error) {
+	f := excelize.NewFile()
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Print("failed to close report")
+			panic(err)
+		}
+	}()
+
+	enable, disable := true, false
+	pic := &excelize.Picture{
+		Extension: ".jpg",
+		File:      image,
+		Format: &excelize.GraphicOptions{
+			PrintObject:     &enable,
+			LockAspectRatio: false,
+			OffsetX:         15,
+			OffsetY:         10,
+			Locked:          &disable,
+		},
+	}
+	if err := f.AddPictureFromBytes("Sheet1", "A1", pic); err != nil {
+		log.Print("failed to add picture")
+		return nil, err
+	}
+
+	const (
+		wordsPerPage int = 180
+		wordsPerLine int = 6
+	)
+
+	text = strings.Repeat(text, 1000)
+
+	var (
+		pageIndex int
+		pageName  string
+		progress  uint32
+		words     []string = strings.Fields(text)
+	)
+
+	wordsCount := len(words)
+	fivePercent := (wordsCount * 5) / 100
+
+	for i := 0; i < len(words); i++ {
+		if i%wordsPerPage == 0 {
+			pageIndex++
+			pageName = fmt.Sprint("Page ", pageIndex)
+			_, err := f.NewSheet(pageName)
+			if err != nil {
+				log.Print("failed to add page")
+				return nil, err
+			}
+			err = f.SetColWidth(pageName, "A", "F", 30)
+			if err != nil {
+				log.Print("failed to set column width")
+				return nil, err
+			}
+		}
+
+		if i%fivePercent == 0 {
+			err := stream.Send(&reporter.GetResponse{
+				Res: &reporter.GetResponse_Status{
+					Status: progress,
+				},
+			})
+			if err != nil {
+				return nil, err
+			}
+			progress += 5
+		}
+
+		col := i%wordsPerLine + 1
+		row := (i/wordsPerLine)%(wordsPerPage/wordsPerLine) + 1
+		cell, err := excelize.CoordinatesToCellName(col, row)
+		if err != nil {
+			return nil, err
+		}
+		f.SetCellValue(pageName, cell, words[i])
+	}
+
+	name := filepath.Join("..", "storage", "reports", fmt.Sprintf("%s.xlsx", time))
+	if err := f.SaveAs(name); err != nil {
+		log.Print("failed to save report")
+		return nil, err
+	}
+	buf, err := f.WriteToBuffer()
+	if err != nil {
+		log.Print("failed to write report to buffer")
+		return nil, err
+	}
+	return buf, nil
 }
 
 func getLocalTime() (string, error) {
@@ -78,13 +241,17 @@ func getLocalTime() (string, error) {
 		log.Print("failed to connect to time service")
 		return "", err
 	}
-	defer conn.Close()
-
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			panic("failed to close file")
+		}
+	}()
 	c := timer.NewTimerClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
-	res, err := c.GetTime(ctx, &timer.GetRequest{Message: message})
+	res, err := c.GetTime(ctx, &emptypb.Empty{})
 	if err != nil {
 		log.Print("failed to get current time")
 		return "", err
@@ -95,8 +262,8 @@ func getLocalTime() (string, error) {
 		log.Print("failed to get local time")
 		return "", err
 	}
-	time := fmt.Sprintf("%02d:%02d:%02d:%v", t.Hour(), t.Minute(), t.Second(), t.Nanosecond())
-	return time, nil
+	ts := fmt.Sprintf("%02d:%02d:%02d:%v", t.Hour(), t.Minute(), t.Second(), t.Nanosecond())
+	return ts, nil
 }
 
 func getTimeInTimeZone(timeZone string, utcTime time.Time) (time.Time, error) {
@@ -108,29 +275,16 @@ func getTimeInTimeZone(timeZone string, utcTime time.Time) (time.Time, error) {
 	return utcTime.In(loc), nil
 }
 
-func sendLocalTime(time string, stream reporter.Reporter_GetReportServer) error {
-	err := stream.Send(&reporter.GetResponse{
-		Value: &reporter.GetResponse_Time{
-			Time: time,
-		},
-	})
-	if err != nil {
-		log.Print("failed to send time")
-		return err
-	}
-	return nil
-}
-
 func getRandomImage() ([]byte, error) {
 	c := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", riUrl, nil)
 	if err != nil {
 		log.Print("failed to create http request")
 		return nil, err
 	}
 
-	req.Header.Add("X-Api-Key", apiKey)
-	req.Header.Add("Accept", accept)
+	req.Header.Add("X-Api-Key", riApiKey)
+	req.Header.Add("Accept", riAccept)
 	resp, err := c.Do(req)
 	if err != nil {
 		log.Print("failed to do http request")
@@ -146,68 +300,55 @@ func getRandomImage() ([]byte, error) {
 	return data, nil
 }
 
-func saveDataToFile(data []byte, filePath string) error {
-	file, err := os.Create(filePath)
+func getFishText() (string, error) {
+	c := &http.Client{}
+	url := fmt.Sprintf("%s?&type=%s&number=%d", ftUrl, ftType, ftNumber)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Print("failed to create file")
-		return err
+		log.Print("failed to create http request")
+		return "", err
 	}
-	defer file.Close()
-
-	n, err := file.Write(data)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := c.Do(req)
 	if err != nil {
-		log.Print("failed to save file")
-		return err
+		log.Print("failed to do http request")
+		return "", err
 	}
-	log.Printf("saved file is %d bytes", n)
-	return nil
-}
-
-func sendDataToStream(data []byte, stream reporter.Reporter_GetReportServer) error {
-	buf := make([]byte, chunkSize)
-	reader := bytes.NewReader(data)
-	chunksSent := 0
-	bytesSent := 0
-
-	for {
-		n, err := reader.Read(buf)
-		if err == io.EOF {
-			break
-		}
+	defer func() {
+		resp.Body.Close()
 		if err != nil {
-			log.Print("failed to read chunk")
-			return err
+			panic("failed to close response body")
 		}
-		err = stream.Send(&reporter.GetResponse{
-			Value: &reporter.GetResponse_Chunk{
-				Chunk: buf[:n],
-			},
-		})
-		if err != nil {
-			log.Print("failed to send bytes")
-			return err
-		}
-		chunksSent++
-		bytesSent += n
+	}()
+
+	decoder := json.NewDecoder(resp.Body)
+	var ftResp ftResponse
+	err = decoder.Decode(&ftResp)
+	if err != nil {
+		log.Print("failed to decode response")
+		return "", err
+	}
+	if ftResp.ErrorCode != 0 {
+		log.Print("failed to get fish text")
+		return "", err
 	}
 
-	log.Printf("Total amount of chunks sent: %d", chunksSent)
-	log.Printf("Total amount of bytes sent: %d", bytesSent)
-	return nil
+	return ftResp.Text, nil
 }
 
 func main() {
+	runtime.SetBlockProfileRate(1)
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
 	if err != nil {
 		log.Fatalf("failed to listen %d: %s", grpcPort, err)
 	}
-
-	s := grpc.NewServer()
-	reflection.Register(s)
-	reporter.RegisterReporterServer(s, &server{})
+	go func() {
+		http.ListenAndServe(":6666", nil)
+	}()
+	gs := grpc.NewServer()
+	reflection.Register(gs)
+	reporter.RegisterReporterServer(gs, &server{})
 
 	log.Printf("reporter is listening at %v", lis.Addr())
-	if err = s.Serve(lis); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(gs.Serve(lis))
 }
--- reporter/go.mod
+++ reporter/go.mod
@@ -3,14 +3,24 @@ module reporter
 go 1.21.1
 
 require (
+	github.com/xuri/excelize/v2 v2.8.0
 	google.golang.org/grpc v1.58.2
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	github.com/google/pprof v0.0.0-20231023181126-ff6d637d2a7b // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20231023195312-e2daf7ba7156 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/xuri/efp v0.0.0-20230802181842-ad255f2331ca // indirect
+	github.com/xuri/nfp v0.0.0-20230819163627-dc951e3ffe1a // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
 )
