e9d11bb38778b2949991f9828f28c265559d7496
Yury Popenko <popenko1337@gmail.com>
2023-10-27 15:39:40 +0700

    add kubernetes configs for program components

--- pinger/cmd/client.go
+++ pinger/cmd/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	pinger "pinger/pkg/reporter/service"
@@ -13,14 +14,24 @@ import (
 )
 
 const (
-	address = "localhost:50052"
-	place   = "Asia/Novosibirsk"
+	pingerAddress   = ":50050"
+	reporterAddress = "reporter-service:50052"
+	place           = "Asia/Novosibirsk"
 )
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	http.HandleFunc("/", handler)
+	log.Println("pinger is listening at", pingerAddress)
+	log.Fatal(http.ListenAndServe(pingerAddress, nil))
+}
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	log.Println("1 new request")
+	conn, err := grpc.Dial(reporterAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("failed to connect to report server: %e", err)
+		w.Write([]byte("failed to connect to report server"))
+		log.Println("failed to connect to report server")
+		return
 	}
 	defer conn.Close()
 
@@ -30,25 +41,34 @@ func main() {
 
 	stream, err := c.GetReport(ctx, &pinger.GetRequest{Location: place})
 	if err != nil {
-		log.Fatalf("failed to get report: %s", err)
+		w.Write([]byte("failed to get report"))
+		log.Println("failed to get report")
+		return
 	}
 
 	for {
 		res, err := stream.Recv()
 		if err != nil {
-			log.Fatal(err)
+			w.Write([]byte("failed to get recieve data"))
+			log.Println("failed to get recieve data")
+			return
 		}
 		st := res.GetStatus()
 		fmt.Printf("%d%%\n", st)
 		if st == 100 {
-			fmt.Println("report generated successfully.")
+			w.Write([]byte("report generated successfully"))
+			log.Println("report generated successfully")
 			break
 		}
 	}
 
 	res, err := stream.Recv()
 	if err != nil {
-		log.Fatal(err)
+		w.Write([]byte("failed to get download link"))
+		log.Println("failed to get download link")
+		return
 	}
-	fmt.Println("Report is available at: ", res.GetUrl())
+	resp := fmt.Sprintf("Report is available at: %s", res.GetUrl())
+	w.Write([]byte(resp))
+	log.Println("report is available at ", resp)
 }
